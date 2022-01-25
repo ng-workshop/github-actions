9f06de461171bbdc1aebc8084d43977247b8747f
Nicolas Grévin <developer@nicolas-grevin.com>
2022-01-25 15:58:21 +0100

    Fix PHPStan and PHPCS

--- docker/symfony/entrypoint.sh
+++ docker/symfony/entrypoint.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env sh
 
-sudo usermod -u "${USER_ID}" symfony
-sudo groupmod -g "${GROUP_ID}" symfony
+if [ -n "${USER_ID}" ] && [ "${USER_ID}" != "$(id -u)" ]
+then
+  sudo usermod -u "${USER_ID}" symfony
+fi
 
-sshpass -p "${FTP_PASSWORD}" ssh -o StrictHostKeyChecking=no "${FTP_USER}@${FTP_HOST}" || true
+if [ -n "${GROUP_ID}" ] && [ "${GROUP_ID}" != "$(id -g)" ]
+then
+  sudo groupmod -g "${GROUP_ID}" symfony
+fi
+
+if [ -n "${FTP_PASSWORD}" ] || [ -n "${FTP_USER}" ] || [ -n "${FTP_HOST}" ]
+then
+  sshpass -p "${FTP_PASSWORD}" ssh -o StrictHostKeyChecking=no "${FTP_USER}@${FTP_HOST}" || true
+fi
 
 exec "$@"
--- docker/vuejs/entrypoint.sh
+++ docker/vuejs/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
 
-sudo usermod -u "${USER_ID}" vuejs
-sudo groupmod -g "${GROUP_ID}" vuejs
+if [ -n "${USER_ID}" ] && [ "${USER_ID}" != "$(id -u)" ]
+then
+  sudo usermod -u "${USER_ID}" vuejs
+fi
+
+if [ -n "${GROUP_ID}" ] && [ "${GROUP_ID}" != "$(id -g)" ]
+then
+  sudo groupmod -g "${GROUP_ID}" vuejs
+fi
 
 if [ -f "/home/vuejs/.bashrc" ]
 then
